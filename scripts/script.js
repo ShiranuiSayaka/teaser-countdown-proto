9bb006311fd3ebbb8bf72832048dad3431217c6e
ShiranuiSayaka <78851822+ShiranuiSayaka@users.noreply.github.com>
2021-04-28 02:28:37 +0900

    Update Loop Time

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,20 @@
 let hh = 99;
 let mm = 59;
-let ss = 20;
-let isShowColon = true;
+let count = 0;
+
+const renderDict = [
+    { ss: 59, isShowColon: false },
+    { ss: 59, isShowColon: true },
+    { ss: 58, isShowColon: false },
+    { ss: 58, isShowColon: true },
+]
 
 const countDownFunc = () => {
-    if (isShowColon) {
-        if (--ss < 0) {
-            ss = 59;
-            if (--mm < 0) {
-                mm = 59;
-                --hh;
-            }
-        }
+    if (++count > renderDict.length - 1) {
+        count = 0;
     }
-    isShowColon = !isShowColon
     const targetElem = document.getElementById("time");
-    targetElem.innerText = `${("00" + hh).slice(-2)}${isShowColon ? ":" : " "}${("00" + mm).slice(-2)}${isShowColon ? ":" : " "}${("00" + ss).slice(-2)}`
+    targetElem.innerText = `${("00" + hh).slice(-2)}${renderDict[count].isShowColon ? ":" : " "}${("00" + mm).slice(-2)}${renderDict[count].isShowColon ? ":" : " "}${("00" + renderDict[count].ss).slice(-2)}`
 }
 
 setInterval(countDownFunc, 500)
